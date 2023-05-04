b7be55410b18415d9e26d3f0328db3d0f2d79cb4
unknown <ozak0s@gmail.com>
2023-05-04 22:18:16 +0300

    mobile fix

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Work from "./components/Work";
+import "./App.css";
 
 function App() {
   return (
